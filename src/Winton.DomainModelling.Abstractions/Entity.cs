425508c204248e53bc82816ad52b0e40f878ca33
Iain Scott <i.scott@winton.com>
2018-06-13 14:50:51 +0100

    Copied Entity type from projects currently using it.

---
+++ src/Winton.DomainModelling.Abstractions/Entity.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Winton.DomainModelling
+{
+    public abstract class Entity<TEntityId> : IEquatable<Entity<TEntityId>>
+        where TEntityId : IEquatable<TEntityId>
+    {
+        protected Entity(TEntityId id)
+        {
+            Id = id;
+        }
+
+        public TEntityId Id { get; }
+
+        public static bool operator ==(Entity<TEntityId> left, Entity<TEntityId> right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Entity<TEntityId> left, Entity<TEntityId> right)
+        {
+            return !Equals(left, right);
+        }
+
+        public bool Equals(Entity<TEntityId> other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            if (GetType() != other.GetType())
+            {
+                return false;
+            }
+
+            return !Id.Equals(default(TEntityId)) && !other.Id.Equals(default(TEntityId)) && Id.Equals(other.Id);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Entity<TEntityId>);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
---
+++ test/Winton.DomainModelling.Abstractions.Tests/EntityTests.cs
@@ -0,0 +1,148 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Winton.DomainModelling
+{
+    public class EntityTests
+    {
+        public sealed class Equality : EntityTests
+        {
+            [Fact]
+            private void ShouldBeFalseWhenIdHasDefaultValue()
+            {
+                var entityA1 = new EntityA(default(int));
+                var entityA2 = new EntityA(default(int));
+
+                bool equal = entityA1.Equals(entityA2);
+
+                equal.Should().BeFalse();
+            }
+
+            [Fact]
+            private void ShouldBeFalseWhenIdsAreDifferentForDifferentReferences()
+            {
+                var entityA1 = new EntityA(1);
+                var entityA2 = new EntityA(2);
+
+                bool equal = entityA1.Equals(entityA2);
+
+                equal.Should().BeFalse();
+            }
+
+            [Fact]
+            private void ShouldBeFalseWhenOtherIsDifferentTypeOfEntity()
+            {
+                var entityA = new EntityA(1);
+                var entityB = new EntityB(1);
+
+                bool equal = entityA.Equals(entityB);
+
+                equal.Should().BeFalse();
+            }
+
+            [Fact]
+            private void ShouldBeFalseWhenOtherIsNull()
+            {
+                var entityA1 = new EntityA(1);
+
+                bool equal = entityA1.Equals(null);
+
+                equal.Should().BeFalse();
+            }
+
+            [Fact]
+            private void ShouldBeTrueWhenIdsAreSameForDifferentReferences()
+            {
+                var entityA1 = new EntityA(1);
+                var entityA2 = new EntityA(1);
+
+                bool equal = entityA1.Equals(entityA2);
+
+                equal.Should().BeTrue();
+            }
+
+            [Fact]
+            private void ShouldBeTrueWhenReferencesAreSame()
+            {
+                var entityA1 = new EntityA(1);
+
+                bool equal = entityA1.Equals(entityA1);
+
+                equal.Should().BeTrue();
+            }
+        }
+
+        public sealed class ObjectEquality : EntityTests
+        {
+            [Fact]
+            private void ShouldBeFalseWhenIdsAreDifferentForDifferentReferences()
+            {
+                var entityA1 = new EntityA(1);
+                var entityA2 = new EntityA(2);
+
+                bool equal = entityA1.Equals((object)entityA2);
+
+                equal.Should().BeFalse();
+            }
+
+            [Fact]
+            private void ShouldBeFalseWhenOtherIsDifferentTypeOfEntity()
+            {
+                var entityA = new EntityA(1);
+                var entityB = new EntityB(1);
+
+                // ReSharper disable once SuspiciousTypeConversion.Global
+                bool equal = entityA.Equals((object)entityB);
+
+                equal.Should().BeFalse();
+            }
+
+            [Fact]
+            private void ShouldBeFalseWhenOtherIsNull()
+            {
+                var entityA1 = new EntityA(1);
+
+                bool equal = entityA1.Equals((object)null);
+
+                equal.Should().BeFalse();
+            }
+
+            [Fact]
+            private void ShouldBeTrueWhenIdsAreSameForDifferentReferences()
+            {
+                var entityA1 = new EntityA(1);
+                var entityA2 = new EntityA(1);
+
+                bool equal = entityA1.Equals((object)entityA2);
+
+                equal.Should().BeTrue();
+            }
+
+            [Fact]
+            private void ShouldBeTrueWhenReferencesAreSame()
+            {
+                var entityA1 = new EntityA(1);
+
+                bool equal = entityA1.Equals((object)entityA1);
+
+                equal.Should().BeTrue();
+            }
+        }
+
+        private sealed class EntityA : Entity<int>
+        {
+            public EntityA(int id)
+                : base(id)
+            {
+            }
+        }
+
+        private sealed class EntityB : Entity<int>
+        {
+            public EntityB(int id)
+                : base(id)
+            {
+            }
+        }
+    }
+}
