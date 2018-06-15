cd1dbc80524d163ceaf342e815676e39cd448d84
Matthew Thornton <m.thornton@winton.com>
2018-06-15 14:24:43 +0100

    Add domain exceptions.

---
+++ test/Winton.DomainModelling.Abstractions.Tests/DomainExceptionTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Winton.DomainModelling
+{
+    public class DomainExceptionTests
+    {
+        public sealed class Message : DomainExceptionTests
+        {
+            [Theory]
+            [InlineData("Test")]
+            [InlineData("Foo")]
+            private void ShouldSetMessageFromConstructor(string message)
+            {
+                var exception = new DomainException(message);
+
+                exception.Message.Should().Be(message);
+            }
+        }
+    }
+}
