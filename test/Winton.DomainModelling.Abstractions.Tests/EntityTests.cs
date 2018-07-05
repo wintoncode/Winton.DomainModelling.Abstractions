45be94d7433526cfb00bf9fc659d51508004dfa1
Iain Scott <i.scott@winton.com>
2018-07-05 11:38:08 +0100

    Clean up for open source.

--- test/Winton.DomainModelling.Abstractions.Tests/EntityTests.cs
+++ test/Winton.DomainModelling.Abstractions.Tests/EntityTests.cs
@@ -1,4 +1,7 @@
-﻿using FluentAssertions;
+﻿// Copyright (c) Winton. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using FluentAssertions;
 using Xunit;
 
 namespace Winton.DomainModelling
