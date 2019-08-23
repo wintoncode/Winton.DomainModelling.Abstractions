75f08cf789cfedbcaf0f9acaeacbf6ab2119b1a2
Marcel Kenlay <m.kenlay@winton.com>
2019-08-23 15:42:22 +0100

    Add new implementation of Error class for a Conflict error

---
+++ src/Winton.DomainModelling.Abstractions/ConflictError.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Winton. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace Winton.DomainModelling
+{
+    /// <inheritdoc />
+    /// <summary>
+    ///     An error indicating that a conflicting entity exists.
+    /// </summary>
+    public class ConflictError : Error
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ConflictError" /> class.
+        /// </summary>
+        /// <param name="detail">The detail that describes the error.</param>
+        /// <returns>A new instance of <see cref="ConflictError" />.</returns>
+        public ConflictError(string detail)
+            : base("Conflict", detail)
+        {
+        }
+    }
+}
